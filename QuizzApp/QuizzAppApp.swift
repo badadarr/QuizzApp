443c475fa184ecdef753b80e4769fb476a137598
Badar Maulana <45702477+badadarr@users.noreply.github.com>
2020-12-17 18:03:21 +0700

    next App

---
+++ QuizzApp/QuizzAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizzAppApp.swift
+//  QuizzApp
+//
+//  Created by badar on 17/12/20.
+//
+
+import SwiftUI
+
+@main
+struct QuizzAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
